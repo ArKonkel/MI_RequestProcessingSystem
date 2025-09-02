5fc7b801e8c8c52fd2819574412ce3bfd0b0169a
ACArtur <Artur@AppComp.io>
2025-09-02 16:04:47 +0200

    init commit

---
+++ src/main/java/de/hsrm/mi/abschlussarbeit/requestProcessingSystem/RequestProcessingSystemApplication.java
@@ -0,0 +1,13 @@
+package de.hsrm.mi.abschlussarbeit.requestProcessingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestProcessingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestProcessingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hsrm/mi/abschlussarbeit/requestProcessingSystem/RequestProcessingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsrm.mi.abschlussarbeit.requestProcessingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestProcessingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
