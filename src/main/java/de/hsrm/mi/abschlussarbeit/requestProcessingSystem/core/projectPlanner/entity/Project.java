3e687c2f0b7fcf1f6b7d5ed4d28cb20754a0e70c
ACArtur <Artur@AppComp.io>
2025-09-04 10:04:50 +0200

    feature: add releationship between Project and Task

--- src/main/java/de/hsrm/mi/abschlussarbeit/requestProcessingSystem/core/projectPlanner/entity/Project.java
+++ src/main/java/de/hsrm/mi/abschlussarbeit/requestProcessingSystem/core/projectPlanner/entity/Project.java
@@ -2,15 +2,18 @@ package de.hsrm.mi.abschlussarbeit.requestProcessingSystem.core.projectPlanner.e
 
 import de.hsrm.mi.abschlussarbeit.requestProcessingSystem.core.entity.ProcessItem;
 import de.hsrm.mi.abschlussarbeit.requestProcessingSystem.core.requestManager.entity.Request;
+import de.hsrm.mi.abschlussarbeit.requestProcessingSystem.core.taskManager.entity.Task;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -26,4 +29,7 @@ public class Project extends ProcessItem {
     @ManyToOne
     @JoinColumn(name = "request_id")
     private Request request;
+
+    @OneToMany(mappedBy = "project")
+    private Set<Task> tasks;
 }
--- src/main/java/de/hsrm/mi/abschlussarbeit/requestProcessingSystem/core/taskManager/entity/Task.java
+++ src/main/java/de/hsrm/mi/abschlussarbeit/requestProcessingSystem/core/taskManager/entity/Task.java
@@ -2,12 +2,10 @@ package de.hsrm.mi.abschlussarbeit.requestProcessingSystem.core.taskManager.enti
 
 import de.hsrm.mi.abschlussarbeit.requestProcessingSystem.core.entity.ProcessItem;
 import de.hsrm.mi.abschlussarbeit.requestProcessingSystem.core.enums.Priority;
+import de.hsrm.mi.abschlussarbeit.requestProcessingSystem.core.projectPlanner.entity.Project;
 import de.hsrm.mi.abschlussarbeit.requestProcessingSystem.core.requestManager.entity.Request;
 import de.hsrm.mi.abschlussarbeit.requestProcessingSystem.support.calendarModule.entity.CalendarEntry;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -34,4 +32,19 @@ public class Task extends ProcessItem {
     @ManyToOne
     @JoinColumn(name = "request_id")
     private Request request;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    /**
+     * Validation is needed because a Task should only be belong to Project or a Request
+     */
+    @PrePersist
+    @PreUpdate
+    private void validateBelonging() {
+        if ((request != null && project != null)) {
+            throw new IllegalStateException("Task is only allowed to belong either to a Request OR a Project");
+        }
+    }
 }
