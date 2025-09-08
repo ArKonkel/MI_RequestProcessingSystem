aea7c341522a4db206707ff7064e36a0ca2713e3
ACArtur <Artur@AppComp.io>
2025-09-08 16:19:06 +0200

    feature: Introduce CalendarParser and Reposutory

--- src/main/java/de/hsrm/mi/abschlussarbeit/requestProcessingSystem/integration/webhookHandler/CalendarParser.java
+++ src/main/java/de/hsrm/mi/abschlussarbeit/requestProcessingSystem/integration/webhookHandler/CalendarParser.java
@@ -1,4 +1,11 @@
 package de.hsrm.mi.abschlussarbeit.requestProcessingSystem.integration.webhookHandler;
 
+
+import de.hsrm.mi.abschlussarbeit.requestProcessingSystem.support.calendarModule.dto.CalendarEntryDto;
+import de.hsrm.mi.abschlussarbeit.requestProcessingSystem.support.calendarModule.dto.IncomingCalendarEntryDto;
+
 public interface CalendarParser {
+
+    CalendarEntryDto parseCalendarEntry(IncomingCalendarEntryDto incomingCalendarEntryDto);
+
 }
--- src/main/java/de/hsrm/mi/abschlussarbeit/requestProcessingSystem/support/calendarModule/repository/CalendarRepository.java
+++ src/main/java/de/hsrm/mi/abschlussarbeit/requestProcessingSystem/support/calendarModule/repository/CalendarRepository.java
@@ -3,5 +3,9 @@ package de.hsrm.mi.abschlussarbeit.requestProcessingSystem.support.calendarModul
 import de.hsrm.mi.abschlussarbeit.requestProcessingSystem.support.calendarModule.entity.Calendar;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CalendarRepository extends JpaRepository<Calendar, Long> {
+
+    List<Calendar> findByEmployeeId(Long employeeId);
 }
