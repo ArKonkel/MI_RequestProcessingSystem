f40822e8a73b746e90a4576487cc3a8322a3f3c8
ACArtur <Artur@AppComp.io>
2025-09-12 08:45:23 +0200

    feature: clean frontend

--- frontend/Workstation/src/views/HomeView.vue
+++ frontend/Workstation/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 import {onMounted} from "vue";
 import {useTaskStore} from "@/stores/taskStore.ts";
+import {Button} from "@/components/ui/button";
 
 const taskStore = useTaskStore();
 
@@ -13,6 +13,8 @@ onMounted(async () => {
 
 <template>
   <main>
-    <TheWelcome />
+    <Button class="text-1xl">Click me</Button>
+    <Button variant="destructive" class="text-1xl">Click me</Button>
+    <Button variant="ghost" class="text-1xl">Click me</Button>
   </main>
 </template>
