b1c6479dfbb9adc2be1d89d2bc47c7d627e2f77c
ako <artur@appcomp.io>
2025-10-10 13:25:33 +0200

    feat: Fix tests

--- src/test/java/de/hsrm/mi/abschlussarbeit/customerRequestProcessingServer/processItem/ProcessItemImplTest.java
+++ src/test/java/de/hsrm/mi/abschlussarbeit/customerRequestProcessingServer/processItem/ProcessItemImplTest.java
@@ -1,5 +1,8 @@
 package de.hsrm.mi.abschlussarbeit.customerRequestProcessingServer.processItem;
 
+import de.hsrm.mi.abschlussarbeit.customerRequestProcessingServer.comment.Comment;
+import de.hsrm.mi.abschlussarbeit.customerRequestProcessingServer.comment.CommentCreateDto;
+import de.hsrm.mi.abschlussarbeit.customerRequestProcessingServer.comment.CommentRepository;
 import de.hsrm.mi.abschlussarbeit.customerRequestProcessingServer.notification.NotificationService;
 import de.hsrm.mi.abschlussarbeit.customerRequestProcessingServer.notification.UserNotificationType;
 import de.hsrm.mi.abschlussarbeit.customerRequestProcessingServer.task.Task;
@@ -14,6 +17,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -29,6 +33,9 @@ class ProcessItemImplTest {
 
     @Mock
     UserService userService;
+
+    @Mock
+    CommentRepository commentRepository;
 
     @InjectMocks
     ProcessItemImpl processItemService;
@@ -61,5 +68,43 @@ class ProcessItemImplTest {
 
         //Check if saved
         verify(processItemRepository).save(item);
+    }
+
+    @Test
+    void addCommentToProcessItem_shouldResolveMentionsAndPublishEvent() {
+        // GIVEN
+        User author = new User();
+        author.setId(99L);
+
+        User mentioned1 = new User();
+        mentioned1.setId(1L);
+        User mentioned2 = new User();
+        mentioned2.setId(42L);
+
+        ProcessItem processItem = new Task();
+        processItem.setId(123L);
+        processItem.setTitle("Test Process");
+
+        String commentText = "Hello @1 and @42!";
+
+        CommentCreateDto dto = new CommentCreateDto(commentText, author.getId());
+
+        when(userService.getUserById(author.getId())).thenReturn(author);
+        when(userService.getUserById(1L)).thenReturn(mentioned1);
+        when(userService.getUserById(42L)).thenReturn(mentioned2);
+        when(processItemRepository.findById(processItem.getId())).thenReturn(Optional.of(processItem));
+        when(commentRepository.save(any(Comment.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        // WHEN
+        processItemService.addCommentToProcessItem(processItem.getId(), dto);
+
+        // THEN
+        verify(notificationService).sendUserNotification((argThat(event ->
+                event.processItemId().equals(processItem.getId()) &&
+                        event.userIdsToNotify().contains(1L) &&
+                        event.userIdsToNotify().contains(42L) &&
+                        event.text().equals(commentText) &&
+                        event.processItemTitle().equals("Test Process")
+        )));
     }
 }
