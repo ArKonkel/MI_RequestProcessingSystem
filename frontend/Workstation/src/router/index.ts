4f8cc8c2d74b14f5978bf0adcc09e2f9b1d92f92
ako <artur@appcomp.io>
2025-10-22 17:03:27 +0200

    feat: Introduce Forbiddenview.

--- frontend/Workstation/src/router/index.ts
+++ frontend/Workstation/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import axios from 'axios' // 💡 Axios importieren
 
 import TaskView from '@/views/TaskViews/TaskView.vue'
@@ -11,6 +11,10 @@ import ProjectView from '@/views/ProjectViews/ProjectView.vue'
 import EmployeeView from '@/views/CapacityPlanningViews/EmployeeView.vue'
 import EmployeeDetailView from '@/views/CapacityPlanningViews/EmployeeDetailView.vue'
 import LoginView from '@/views/AuthenticationViews/LoginView.vue'
+import {useUserStore} from "@/stores/userStore.ts";
+import type {UserDtd} from "@/documentTypes/dtds/UserDtd.ts";
+import {Role} from "@/documentTypes/types/Role.ts";
+import ForbiddenView from "@/views/AuthenticationViews/ForbiddenView.vue";
 
 const securityEnabled = import.meta.env.VITE_SECURITY_ENABLED === 'true'
 
@@ -37,6 +41,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/forbidden',
+      name: 'Forbidden',
+      component: ForbiddenView,
+
+    },
+    {
       path: '/login',
       name: 'Login',
       component: LoginView,
@@ -49,11 +59,13 @@ const router = createRouter({
       path: '/requests',
       name: 'requestView',
       component: RequestView,
+      meta: {roles: [Role.ADMIN, Role.CUSTOMER_REQUEST_REVISER, Role.TASK_REVISER, Role.CAPACITY_PLANNER, Role.PROJECT_PLANNER]},
       children: [
         {
           path: ':requestId',
           name: 'requestDetailView',
           component: RequestDetailView,
+          meta: {roles: [Role.ADMIN, Role.CUSTOMER_REQUEST_REVISER, Role.TASK_REVISER, Role.CAPACITY_PLANNER, Role.PROJECT_PLANNER]},
         },
       ],
     },
@@ -61,11 +73,13 @@ const router = createRouter({
       path: '/projects',
       name: 'projectView',
       component: ProjectView,
+      meta: {roles: [Role.ADMIN, Role.PROJECT_PLANNER]},
       children: [
         {
           path: ':projectId',
           name: 'projectDetailView',
           component: ProjectDetailView,
+          meta: {roles: [Role.ADMIN, Role.PROJECT_PLANNER]},
         },
       ],
     },
@@ -73,11 +87,13 @@ const router = createRouter({
       path: '/tasks',
       name: 'taskView',
       component: TaskView,
+      meta: {roles: [Role.ADMIN, Role.CUSTOMER_REQUEST_REVISER, Role.TASK_REVISER, Role.CAPACITY_PLANNER, Role.PROJECT_PLANNER]},
       children: [
         {
           path: ':taskId',
           name: 'taskDetailView',
           component: TaskDetailView,
+          meta: {roles: [Role.ADMIN, Role.CUSTOMER_REQUEST_REVISER, Role.TASK_REVISER, Role.CAPACITY_PLANNER, Role.PROJECT_PLANNER]},
         },
       ],
     },
@@ -85,11 +101,13 @@ const router = createRouter({
       path: '/employees',
       name: 'employeeView',
       component: EmployeeView,
+      meta: {roles: [Role.ADMIN, Role.CAPACITY_PLANNER]},
       children: [
         {
           path: ':employeeId',
           name: 'employeeDetailView',
           component: EmployeeDetailView,
+          meta: {roles: [Role.ADMIN, Role.CAPACITY_PLANNER]},
         },
       ],
     },
@@ -97,6 +115,7 @@ const router = createRouter({
       path: '/capacityPlanning/:taskId',
       name: 'capacityPlanningView',
       component: TaskCapacityPlanningView,
+      meta: {roles: [Role.ADMIN, Role.CAPACITY_PLANNER]},
     },
   ],
 })
@@ -111,12 +130,34 @@ router.beforeEach((to, from, next) => {
   const authRequired = !publicPages.includes(to.path) // check if route is public page
 
   if (authRequired && !token) {
-    next('/login')
+    return next('/login')
   } else if (to.path === '/login' && token) {
-    next('/')
-  } else {
-    next()
+    return next('/')
   }
+
+  const userStore = useUserStore()
+  const user = userStore.user as UserDtd
+  const allowedRoles = to.meta.roles as Role[]
+
+  if (!allowedRoles || allowedRoles.length === 0) {
+    return next()
+  }
+
+  //admin is allowed to do everything
+  const isAdmin = user?.roles?.some(role => role.name === Role.ADMIN)
+  if (isAdmin) {
+    return next()
+  }
+
+
+ // Check if user has role
+  const hasRole = user?.roles?.some(role => allowedRoles.includes(role.name))
+  if (!hasRole) {
+    console.error("FORBIDDEN")
+    return next('/forbidden')
+  }
+
+  next()
 })
 
 export default router
