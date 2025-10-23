0ac9127c2dd879e4d50ffbf7c3b5208b4ec63cda
ACArtur <Artur@AppComp.io>
2025-10-23 15:01:14 +0200

    feat: Add Javadoc for NotificationServiceImpl

--- src/main/java/de/hsrm/mi/abschlussarbeit/customerRequestProcessingServer/notification/NotificationServiceImpl.java
+++ src/main/java/de/hsrm/mi/abschlussarbeit/customerRequestProcessingServer/notification/NotificationServiceImpl.java
@@ -12,6 +12,15 @@ public class NotificationServiceImpl implements NotificationService {
 
     private final SimpMessagingTemplate messagingTemplate;
 
+    /**
+     * Sends a user notification based on the specified event.
+     * The method determines the type of the notification and sends
+     * it to the appropriate messaging topic.
+     *
+     * @param event the user notification event containing information
+     *              about the type of notification, the target process item,
+     *              and any additional details for creating the notification
+     */
     @Override
     public void sendUserNotification(UserNotificationEvent event) {
         log.info("Sending notification event {}", event);
@@ -27,6 +36,15 @@ public class NotificationServiceImpl implements NotificationService {
         }
     }
 
+    /**
+     * Sends a change notification based on the provided event. The method identifies
+     * the target type of the change and broadcasts the event to the corresponding
+     * messaging topic.
+     *
+     * @param event the change notification event containing details about the type
+     *              of change, the target entity of the change, and additional
+     *              information to notify relevant subscribers
+     */
     @Override
     public void sendChangeNotification(ChangeNotificationEvent event) {
         log.info("Sending change message event {}", event);
