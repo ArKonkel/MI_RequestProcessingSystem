57901383b8b44f6b6dc719a3e374e18e5c435e77
ACArtur <Artur@AppComp.io>
2025-10-23 15:04:38 +0200

    feat: Add Javadoc for RoleServiceImpl

--- src/main/java/de/hsrm/mi/abschlussarbeit/customerRequestProcessingServer/role/RoleServiceImpl.java
+++ src/main/java/de/hsrm/mi/abschlussarbeit/customerRequestProcessingServer/role/RoleServiceImpl.java
@@ -12,6 +12,14 @@ public class RoleServiceImpl implements RoleService {
 
     private final RoleRepository roleRepository;
 
+    /**
+     * Retrieves a {@link Role} entity by its name. If no role is found with the given name,
+     * a {@link NotFoundException} is thrown.
+     *
+     * @param name the name of the role to retrieve
+     * @return the {@link Role} associated with the provided name
+     * @throws NotFoundException if a role with the specified name is not found
+     */
     @Override
     public Role getRoleByName(String name) {
         return roleRepository.findByName(name).orElseThrow(() -> new NotFoundException("Role with name " + name + " not found"));
