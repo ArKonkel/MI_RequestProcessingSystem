f9b41f5429a556013411e93b68516b4d5e144b57
ACArtur <Artur@AppComp.io>
2025-10-30 11:13:04 +0100

    feat: Change text

--- frontend/Workstation/src/views/AuthenticationViews/ForbiddenView.vue
+++ frontend/Workstation/src/views/AuthenticationViews/ForbiddenView.vue
@@ -3,7 +3,7 @@
     <h1 class="text-6xl font-bold text-red-600 ">403</h1>
     <h2 class="text-2xl font-semibold mb-2">Zugriff verweigert</h2>
     <p class="text-gray-600 mb-6 text-center">
-      Du hast leider keine Berechtigung, diese Seite zu sehen.
+      Sie haben leider keine Berechtigung, diese Seite zu sehen.
     </p>
     <router-link to="/" class="px-4 py-2 bg-blue-600 text-white rounded ">
       Zur Startseite
